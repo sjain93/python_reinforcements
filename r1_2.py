5fad4e28019481ae3ed95bb1c75deb9c1c3e7bd4
Sanchit Jain <sanchit.jain@mail.mcgill.ca>
2019-02-12 14:56:22 -0500

    stretch exercise

--- r1_2.py
+++ r1_2.py
@@ -13,17 +13,20 @@ print("Do you like Comedy? Rate between 1-5")
 ans3 = float(input())
 
 
-if ans1 >= 4 and ans2 < 4 and ans3 < 4:
+if ans1 >= 4:
     print("I would recommend {}".format(doc))
-elif ans1 >= 4 and ans2 >= 4 and ans3 < 4:
-    print("I would recommend {}, and {}".format(doc, drama))
-elif ans1 < 4 and ans2 >= 4 and ans3 >= 4:
+elif ans1 <= 3 and ans2 >= 4 and ans3 >= 4:
     print("I would recommend {}".format(dramed))
 elif ans1 < 4 and ans2 >= 4 and ans3 < 4:
     print("I would recommend {}".format(drama))
 elif ans1 < 4 and ans2 < 4 and ans3 >= 4:
     print("I would recommend {}".format(comedy))
-elif ans1 < 4 and ans2 < 4 and ans3 < 4:
-    print("Try reading A Game of Thrones instead!")
+elif ans1 <= 3 and ans2 <= 3 and ans3 <= 3:
+    if (ans1 > ans2) & (ans1 > ans3):
+        print("I would recommend {}".format(doc))
+    elif (ans2 > ans1) & (ans2 > ans3):
+        print("I would recommend {}".format(drama))
+    elif (ans3 > ans1) & (ans3 > ans2):
+        print("I would recommend {}".format(comedy))
 else:
-    print("You're way to picky for this program")
+    print("Try reading A Game of Thrones instead!")
