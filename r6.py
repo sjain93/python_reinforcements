7445618dfd4a22391ee9ba209709d77415fec7aa
Sanchit Jain <sanchit.jain@mail.mcgill.ca>
2019-02-20 14:22:13 -0500

    Reinforcement complete

--- r6.py
+++ r6.py
@@ -1,3 +1,4 @@
+#Ex 1
 def odd_sum_return(num_list):
     odd_sum = 0
     for number in num_list:
@@ -7,3 +8,18 @@ def odd_sum_return(num_list):
 
 test = [0,1,2,3,4,5,6]
 print(odd_sum_return(test))
+
+#Ex 2
+
+name_list = ["James", "Harry", "Lily"]
+
+print("Please input a name and I shall check and see if the name is part of the list")
+query = str(input())
+
+for item in name_list:
+    if query == item:
+        print("Hello {}".format(query))
+        break
+    else:
+        print("Who goes there?")
+        break
