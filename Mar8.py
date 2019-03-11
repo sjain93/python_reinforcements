525e6fb09f292afbd998637f07e632021be7537e
Sanchit Jain <sanchit.jain@mail.mcgill.ca>
2019-03-11 10:08:34 -0400

    This is code for reinforcement

---
+++ Mar8.py
@@ -0,0 +1,31 @@
+ballots = [{'gold': 'Smudge', 'silver': 'Tigger', 'bronze': 'Simba'},
+           {'gold': 'Bella', 'silver': 'Lucky', 'bronze': 'Tigger'},
+           {'gold': 'Bella', 'silver': 'Boots', 'bronze': 'Smudge'},
+           {'gold':'Boots', 'silver': 'Felix', 'bronze': 'Bella'},
+           {'gold': 'Lucky', 'silver': 'Felix', 'bronze': 'Bella'},
+           {'gold': 'Smudge', 'silver': 'Simba', 'bronze': 'Felix'}]
+
+tally = {}
+
+def tally_ballots(ballots, tally):
+    for ballot in ballots:
+        gname, sname, bname = ballot['gold'], ballot['silver'], ballot['bronze']
+        # add gold votes
+        tally[gname] = tally.get(gname, 0) + 3
+        # add silver votes
+        tally[sname] = tally.get(sname, 0) + 2
+        # add bronze votes (longer way to do it)
+        if bname in tally:
+            tally[bname] += 1
+        else:
+            tally[bname] = 1
+#
+if __name__ == '__main__':
+    def pr_fn(s):
+        print(f"Printer function says '{s}'")
+    def pr(fn, s):
+        fn(s)
+    pr(pr_fn, 'what is this madness')
+else:
+    tally_ballots(ballots, tally)
+    print(tally)
